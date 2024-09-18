e63ecd6b391d311f150c6f6e369936473baa288a
jnavarrow2f <javier.navarro.w2fly@gmail.com>
2024-09-18 17:20:29 +0200

    Ok

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,8 @@ import {importProvidersFrom} from '@angular/core';
 import {bootstrapApplication} from '@angular/platform-browser';
 import {provideHttpClient} from '@angular/common/http';
 import {provideAnimations} from '@angular/platform-browser/animations';
-import {VERSION as CDK_VERSION} from '@angular/cdk';
-import {VERSION as MAT_VERSION, MatNativeDateModule} from '@angular/material/core';
+import {MatNativeDateModule} from '@angular/material/core';
 import {DialogContentExample} from './example/dialog-content-example';
-
-/* eslint-disable no-console */
-console.info('Angular CDK version', CDK_VERSION.full);
-console.info('Angular Material version', MAT_VERSION.full);
 
 bootstrapApplication(DialogContentExample, {
   providers: [
@@ -18,4 +13,3 @@ bootstrapApplication(DialogContentExample, {
     importProvidersFrom(MatNativeDateModule)
   ]
 }).catch(err => console.error(err));
-
